9d6ee689720c5a29dd24837b8da12215a5203081
Atinuke Ayangade <aayangade@gmail.com>
2023-12-25 13:27:26 -0500

    dashboard page with cards, revenuechart, and latest invoices

--- nextjs-dashboard/next.config.js
+++ nextjs-dashboard/next.config.js
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    };
 
 module.exports = nextConfig;
+
+
+module.exports = {
+
+    // Can be safely removed in newer versions of Next.js
+  
+      // by default, if you customize webpack config, they switch back to version 4.
+      // Looks like backward compatibility approach.
+      webpack5: true,   
+  
+    webpack(config) {
+      config.resolve.fallback = {
+  
+        // if you miss it, all the other options in fallback, specified
+        // by next.js will be dropped.
+        ...config.resolve.fallback,  
+  
+        fs: false, // the solution
+      };
+      
+      return config;
+    },
+  };
+  module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+      ],
+    },
+  };
